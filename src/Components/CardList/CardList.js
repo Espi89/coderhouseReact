468e2eee576f5235c927d498c2736a1f15c45122
Espi89 <marceloespinosa1989@gmail.com>
2022-03-17 01:23:48 -0300

    subida al servidor

---
+++ src/Components/CardList/CardList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CardList = () => {
+  return (
+    <div>Listado de los elementos y api
+        Hacemos la peticion asincrona
+        
+    </div>
+  )
+}
+
+export default CardList
